003ad5724a54c2acccc522547f3354637fc0b81a
Strand McCutchen <strand@nird.us>
2013-04-16 08:33:49 -0700

    New Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseworkGizmo::Application.config.secret_token = 'ec292b90412b468bb1b1dafed49b933e8dab7455b4cd52e5635635651408c321a40ee05b2968c93e5a5ee7f06f68ecf17a432e90b2d491577324685cc574fbff'
