c2ef6278ef56ddc707df1b01961f7f3fd29e60bf
Strand McCutchen <strand@nird.us>
2013-06-02 21:27:03 -0700

    Locked down app to require signed in users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_access
+
+  protected
+  def check_access
+    return if request.env['PATH_INFO'] == new_member_registration_path ||
+              request.env['PATH_INFO'] == member_session_path
+    redirect_to member_session_path and return unless member_signed_in?
+  end
 end
